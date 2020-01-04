06d727f103a43f71058fb4cd67157540e3eccdd9
Esther Walker <equestriennesrule@yahoo.com>
2020-01-04 13:48:09 -0800

    added javascript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+var searchButton = document.getElementById('search');
+var clearResults = document.getElementById('clear-results');
+var searchTerm = document.querySelector('examplesearchterm');
+var recordsToRetrieve = document.querySelector('recordinput');
+var startYear = document.querySelector('start-the-year');
+var endYear = document.querySelector('end-the-year');
+
+searchButton.addEventListener('click', function () {
+    localStorage.setitem('search');
+})
 
